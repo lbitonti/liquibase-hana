1ef6bfed0e1a7ff8ed78123b7426013ec323578e
Benjamin Kastelic <benjamin.kastelic@cloud.si>
2016-10-21 22:27:42 +0200

    added modify data type generator for SAP HANA

---
+++ src/main/java/liquibase/sqlgenerator/ext/ModifyDataTypeGeneratorHanaDB.java
@@ -0,0 +1,72 @@
+package liquibase.sqlgenerator.ext;
+
+import liquibase.database.Database;
+import liquibase.database.ext.HanaDBDatabase;
+import liquibase.datatype.DataTypeFactory;
+import liquibase.datatype.DatabaseDataType;
+import liquibase.sql.Sql;
+import liquibase.sql.UnparsedSql;
+import liquibase.sqlgenerator.SqlGeneratorChain;
+import liquibase.sqlgenerator.core.ModifyDataTypeGenerator;
+import liquibase.statement.core.InsertOrUpdateStatement;
+import liquibase.statement.core.ModifyDataTypeStatement;
+
+/**
+ * Created by Benjamin on 21. okt. 2016.
+ */
+public class ModifyDataTypeGeneratorHanaDB extends ModifyDataTypeGenerator {
+
+    @Override
+    public boolean supports(ModifyDataTypeStatement statement, Database database) {
+        return database instanceof HanaDBDatabase;
+    }
+
+    @Override
+    public int getPriority() {
+        return PRIORITY_DATABASE;
+    }
+
+    @Override
+    public Sql[] generateSql(ModifyDataTypeStatement statement, Database database, SqlGeneratorChain sqlGeneratorChain) {
+        if (!supports(statement, database)) {
+            return sqlGeneratorChain.generateSql(statement, database);
+        }
+
+        StringBuffer buffer = new StringBuffer();
+        buffer
+                .append("ALTER TABLE ")
+                .append(database.escapeSequenceName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName()))
+                .append(" ALTER (")
+                .append(database.escapeColumnName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName(), statement.getColumnName()))
+                .append(" ");
+
+        DatabaseDataType newDataType = DataTypeFactory.getInstance().fromDescription(statement.getNewDataType(), database).toDatabaseDataType(database);
+        buffer
+                .append(newDataType)
+                .append(")");
+
+//        String alterTable = "ALTER TABLE " + database.escapeTableName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName());
+//
+//        // add "MODIFY"
+//        alterTable += " " + getModifyString(database) + " ";
+//
+//        // add column name
+//        String columnName = database.escapeColumnName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName(), statement.getColumnName());
+//        alterTable += columnName;
+//
+//        alterTable += getPreDataTypeString(database); // adds a space if nothing else
+//
+//        // add column type
+//        DatabaseDataType newDataType = DataTypeFactory.getInstance().fromDescription(statement.getNewDataType(), database).toDatabaseDataType(database);
+//
+//        alterTable += newDataType;
+//
+//        if (database instanceof PostgresDatabase) {
+//            alterTable += " USING ("+columnName+"::"+newDataType+")";
+//        }
+
+        String sql = buffer.toString();
+
+        return new Sql[]{new UnparsedSql(sql, getAffectedTable(statement))};
+    }
+}
